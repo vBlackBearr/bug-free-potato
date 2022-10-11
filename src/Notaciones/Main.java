5ae3541b9849587f0e707e8713a575f843f2c9b0
Juan Oloarte <JuanOL_23@outlook.es>
2022-10-10 20:39:58 -0500

    nada

---
+++ src/Notaciones/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Notaciones;
+
+/**
+ *
+ * @author JuanO
+ */
+public class Main {
+    public static void main(String[] args){
+        NotacionPolaca np = new NotacionPolaca();
+        String infijo="4+5*9/8";
+        String operacion = np.notacionPolaca(infijo, "variables");
+        System.out.println(operacion);
+    }
+}
